9e23779b79817826d6b2b57b272d7a4a6e61a4a3
Krzysztof Król <git@royalcode.eu>
2019-12-16 21:34:52 +0100

    Gotowy moduł usług

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import './App.css';
-import { Container, Row, Col } from 'reactstrap'; 
-
-import service from './content'
+import './css/App.css';
+import logo from './images/royalcode-logo.svg';
+import Services from './components/Services';
 
 
 function App() {
@@ -10,36 +9,11 @@ function App() {
     <div className="App">
       <header className="App-header d-flex align-items-center">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="mb-3">Koszt strony internetowej</h1>
-        <p className="small mb-5">
-          Wybierz usługę, która Cię interesuje.
-        </p>
-        <KindOfWork />
+        <h1 className="mb-3">Wycena usług programistycznych</h1>
+        <Services />
         {/* <Materials /> */}
       </header>
     </div>
   );
 }
 export default App;
-
-
-const Materials = () => {
-  return (
-    <Container>
-      <Row>
-        <Col sm="12" md={{ size: 8, offset: 2  }} lg={{ size: 8, offset: 2 }}>
-          <p className="small">Jeśli posiadasz materiały potrzebne do stworzenia strony (teksty, zdjęcia lub logo) możemy szybciej zacząć,
-  a co za tym idzie szybciej skończyć pracę nad stroną internetową.</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col >
-          Mam wszystkie materiały  
-        </Col>
-        <Col >
-          Będę dosyłał materiały w trakcie realizacji
-        </Col>
-      </Row>
-    </Container>
-  );
-}
--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,5 +1,5 @@
+import React from 'react';
 import { Container, Row, Col } from 'reactstrap'; 
-import logo from './royalcode-logo.svg';
 import WebIcon from '@material-ui/icons/Web';
 import StorefrontIcon from '@material-ui/icons/Storefront';
 import SpeedIcon from '@material-ui/icons/Speed';
@@ -8,23 +8,46 @@ import BuildIcon from '@material-ui/icons/Build';
 import AppsIcon from '@material-ui/icons/Apps';
 import CodeIcon from '@material-ui/icons/Code';
 
+const Service = (props) => {
+  return (
+    <Col sm="12" md="6" xl="3">
+      {props.children}
+      <p>{props.serviceName}</p>
+    </Col>
+  );
+}
 
 const Services = () => {
-    return (
-      <Container className="has-icons mb-5">
-        <Row className="d-flex flex-row justify-content-center">
-            
-        </Row>
-      </Container>
-    );
-  }
-  
-  const Service = (props) => {
-    return (
-      <Col sm="12" md="6" xl="3">
-        <StorefrontIcon />
-        <p>Sklep internetowy</p>
-      </Col>
-    );
-  }
+  return (
+    <Container className="has-icons mb-5">
+      <p className="small mb-5">
+        Wybierz temat, który Cię interesuje.
+      </p>
+      <Row className="d-flex flex-row justify-content-center">
+        <Service serviceName="Strona internetowa">
+          <WebIcon />
+        </Service>
+        <Service serviceName="Sklep internetowy">
+          <StorefrontIcon />
+        </Service>
+        <Service serviceName="Optymalizacja SEO">
+          <SpeedIcon />
+        </Service>
+        <Service serviceName="Pozycjonowanie">
+          <TrendingUpIcon />
+        </Service>
+        <Service serviceName="Naprawa strony">
+          <BuildIcon />
+        </Service>
+        <Service serviceName="Aplikacja internetowa">
+          <AppsIcon />
+        </Service>
+        <Service serviceName="Inne zlecenie">
+          <CodeIcon />
+        </Service>
+      </Row>
+    </Container>
+  );
+}
+export default Services;
   
